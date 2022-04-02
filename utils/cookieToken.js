cd9ba11c5b3af056debc45dff43712dc1ed7b641
aniruddhparwal <aniruddhparwal@gmail.com>
2022-04-02 16:25:15 +0530

    try cookie

--- utils/cookieToken.js
+++ utils/cookieToken.js
@@ -1,21 +1,19 @@
 const cookieToken = async (user, res) => {
-    const token = await user.getJwtToken()
-    const options = {
-        expires: new Date(
-            Date.now() + process.env.COOKIE_TIME
-        ),
-        httpOnly: true
-    }
+  const token = await user.getJwtToken();
+  const options = {
+    expires: new Date(Date.now() + process.env.COOKIE_TIME),
+    httpOnly: true,
+    domain: "https://task-managemnt.netlify.app",
+  };
 
-    user.password = undefined
-    console.log("token", token)
+  user.password = undefined;
+  console.log("token", token);
 
-    res.status(200).cookie('token', token, options).json({
-        success: true,
-        token,
-        user,
-    })
-}
+  res.status(200).cookie("token", token, options).json({
+    success: true,
+    token,
+    user,
+  });
+};
 
-
-module.exports = cookieToken
+module.exports = cookieToken;
