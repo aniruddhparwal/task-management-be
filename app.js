f23e3e6a59cda5d1e32b9bb97889a1bff07d7c95
aniruddhparwal <aniruddhparwal@gmail.com>
2022-04-02 15:22:36 +0530

    access control

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
+    "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
 });
